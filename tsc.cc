8b716b7df98cd8639bc131f130c46e2951debd34
ezrabartlett <ezrabartlett@tamu.edu>
2020-02-11 16:39:47 -0600

    username

--- tsc.cc
+++ tsc.cc
@@ -88,7 +88,11 @@ int Client::connectTo()
 
     User current_user; 
     current_user.set_username(username);
+    
+    ReplyStatus login_statustat;
+    Status stat = stub_->login(&context, current_user, &login_status);
 
+    std::cout << ReplyStatus.status;
     return 1; // return 1 if success, otherwise return -1
 }
 
